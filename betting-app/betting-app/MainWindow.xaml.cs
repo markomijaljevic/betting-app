d846b574863ed92f0d6646d8b8ec7ba7ff21b652
markomijaljevic <markomijaljevic96@gmail.com>
2018-09-10 07:30:46 +0200

    MVVM model

--- betting-app/betting-app/MainWindow.xaml.cs
+++ betting-app/betting-app/MainWindow.xaml.cs
@@ -23,38 +23,14 @@ namespace betting_app
 
     public partial class MainWindow : Window
     {
+
         public MainWindow()
         {
             InitializeComponent();
-            Loaded += MainWindow_Loaded;
+            this.DataContext = new MainViewModel(this);
         }
 
-        private void MainWindow_Loaded(object sender, EventArgs e)
-        {
-            databaseEntities context = new databaseEntities();
-            ObservableCollection<table_match> match = new ObservableCollection<table_match>();
-            foreach (table_match m in context.table_match)
-            {
-                match.Add(new table_match
-                {
-
-                    Id = m.Id,
-                    home_team = m.home_team + " - " + m.away_tema,
-                    star__of_the_match = m.star__of_the_match,
-                    home_team_odds = m.home_team_odds,
-                    even_odds = m.even_odds,
-                    away_team_odds = m.away_team_odds,
-                    home_team_or_even_odds = m.home_team_or_even_odds,
-                    away_team_or_even_odds = m.away_team_or_even_odds,
-                    home_or_away_team_odds = m.home_or_away_team_odds,
-                    handicap_odds = m.handicap_odds
-
-                });
-            }
-
-            dataGrid.ItemsSource = match;
-        }
-
+       
     }//end of class
 }//end of namespace
 
